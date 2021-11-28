34f3952387a9ed36a85f7232e7bfadca9760fae8
mahmouddiaa13 <mahmouddiaa13@gmail.com>
2021-11-28 23:42:25 +0200

    Fibonacci series using Generators

---
+++ fibonacci_generators.py
@@ -0,0 +1,5 @@
+def fib(num):
+    a, b = 0, 1
+    for i in range(0, num):
+        yield "{} : {}".format(i+1, a)
+        a, b = b, a + b
---
+++ main.py
@@ -0,0 +1,4 @@
+from fibonacci_generators import fib
+# fibonacci series using Generator:
+for item in fib(10):
+    print(item)
